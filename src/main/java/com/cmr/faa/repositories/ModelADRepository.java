6133a74441ea5585c2a9f846d365704649a37cc3
Cameron Roe <croe10@ford.com>
2019-05-14 13:53:05 -0400

    Potential working common

--- src/main/java/com/cmr/faa/repositories/ModelADRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cmr.faa.repositories;
-
-import com.cmr.faa.DAO.Mapping.ModelToADMappingDAO;
-import org.springframework.data.repository.Repository;
-
-public interface ModelADRepository extends Repository<ModelToADMappingDAO, Integer> {
-  ModelToADMappingDAO save(ModelToADMappingDAO modelToADMappingDAO);
-}
