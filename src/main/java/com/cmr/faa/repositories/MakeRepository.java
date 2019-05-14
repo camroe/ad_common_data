04fcacaf531c6bd69f3c1fb12f114c8f3e08e35c
Cameron Roe <croe10@ford.com>
2019-05-14 15:40:24 -0400

    Moved repositories to ad_common_data

---
+++ src/main/java/com/cmr/faa/repositories/MakeRepository.java
@@ -0,0 +1,14 @@
+package com.cmr.faa.repositories;
+
+import com.cmr.faa.DAO.makes.MakeDao;
+import org.springframework.data.repository.Repository;
+
+import java.util.List;
+
+public interface MakeRepository extends Repository<MakeDao, Integer> {
+  List<MakeDao> findAll();
+
+//    MakeDao findByMake_id(Integer make_id);
+
+  MakeDao save(MakeDao adToPersist);
+}
---
+++ src/main/java/com/cmr/faa/repositories/ModelADRepository.java
@@ -0,0 +1,8 @@
+package com.cmr.faa.repositories;
+
+import com.cmr.faa.DAO.Mapping.ModelToADMappingDAO;
+import org.springframework.data.repository.Repository;
+
+public interface ModelADRepository extends Repository<ModelToADMappingDAO, Integer> {
+  ModelToADMappingDAO save(ModelToADMappingDAO modelToADMappingDAO);
+}
